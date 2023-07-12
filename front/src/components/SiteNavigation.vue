0b337b1c18f2608df65e82ff0ad602bbd68c30c6
TheJocker94 <danielecastellano94@gmail.com>
2023-07-12 03:38:34 -0500

    login

--- front/src/components/SiteNavigation.vue
+++ front/src/components/SiteNavigation.vue
@@ -8,11 +8,13 @@
           <img src="@/assets/ponghino.png" class="h-10" alt="logo" />
         </div>
       </RouterLink>
-      <div class="flex gap-3 flex-1 justify-end">
-        <i
-          class="fa-solid fa-right-to-bracket text-xl text-yellow-400 hover:text-red-500 duration-150 cursor-pointer"
-        ></i>
-      </div>
+      <RouterLink :to="{ name: 'login' }">
+        <div class="flex gap-3 flex-1 justify-end">
+          <i
+            class="fa-solid fa-right-to-bracket text-xl text-yellow-400 hover:text-red-500 duration-150 cursor-pointer"
+          ></i>
+        </div>
+      </RouterLink>
     </nav>
   </header>
 </template>
---
+++ front/src/views/LoginView.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="login"></div>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from "vue-class-component";
+
+@Options({
+  components: {},
+})
+export default class LoginView extends Vue {}
+</script>
