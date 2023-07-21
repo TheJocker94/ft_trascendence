f5fc463133cf5d68fba86cb587acd06c9bd928f5
TheJocker94 <danielecastellano94@gmail.com>
2023-07-21 12:42:04 +0200

    base fatta meglio

---
+++ frontgame/src/views/GameView.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import GamePong from '@/components/GamePong.vue'
+import { RouterLink } from 'vue-router'
+</script>
+
+<template>
+  <div>
+    <RouterLink to="/"><button>Home</button></RouterLink>
+  </div>
+  <GamePong />
+</template>
+
+<style>
+</style>
---
+++ frontgame/src/views/HomeView.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import { RouterLink } from 'vue-router'
+</script>
+
+<template>
+<div>
+  <h1>Home</h1>
+  <RouterLink :to="{ name: 'game' }"><button>Game</button></RouterLink>
+</div>
+</template>
