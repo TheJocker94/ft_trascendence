a4ca6dc003a7c2c5a07afad613ec73ce837a7234
TheJocker94 <danielecastellano94@gmail.com>
2023-07-23 11:51:10 +0200

    prove prisma

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # ft_trascendence
 42 project pong site
+
+Backend:
+Following guide at :[Title](https://www.prisma.io/blog/nestjs-prisma-rest-api-7D056s1BmOL0#introduction)
+Step: Add PrismaClient to the Articles module
+
+Frontend:
+Gioco finito, da implementare multiplayer(Socket.io o Nest, non so)
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,14 +1,37 @@
+// import { NestFactory } from '@nestjs/core';
+// import { AppModule } from './app.module';
+// import * as cors from 'cors';
+
+// async function bootstrap() {
+//   const app = await NestFactory.create(AppModule);
+//   app.use(cors());
+//   await app.listen(3000);
+// }
+// bootstrap();
+
+// src/main.ts
+
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import * as cors from 'cors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(cors());
+
+  const config = new DocumentBuilder()
+    .setTitle('Median')
+    .setDescription('The Median API description')
+    .setVersion('0.1')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
-
 // Per farlo funzionare ho usato cors
 // Please note that enabling CORS without any specific configuration (as shown above) will allow any origin to access your backend. In a production environment, you should consider setting up more fine-grained CORS configurations
 //based on your specific needs for security.
