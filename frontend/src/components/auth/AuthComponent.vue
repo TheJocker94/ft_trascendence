6ae251889ebc60262d25ea31a3095f959eb7e265
TheJocker94 <danielecastellano94@gmail.com>
2023-08-08 13:26:51 +0200

    bottoni meno di colore

--- frontend/src/components/auth/AuthComponent.vue
+++ frontend/src/components/auth/AuthComponent.vue
@@ -11,7 +11,7 @@
             </svg>
             Sign in with Google
           </button>
-          <button @click="signin.login()" type="button" class="text-black bg-[#e6e6e6] hover:bg-[#050708]/90 focus:ring-4 focus:outline-none focus:ring-[#050708]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-[#050708]/50 dark:hover:bg-[#050708]/30 mr-2 mb-2">
+          <button @click="signin.login()" type="button" class="text-black bg-[#e6e6e6] hover:bg-[#f2f2f2]/90 focus:ring-4 focus:outline-none focus:ring-[#f2f2f2]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-[#f2f2f2]/50 dark:hover:bg-[#f2f2f2]/30 mr-2 mb-2">
             <img class="object-scale-down h-5 w-5 mr-2" src="src/assets/42logo.png">
             Sign in with Intra 42
           </button>
--- frontend/src/components/auth/SignupModal.vue
+++ frontend/src/components/auth/SignupModal.vue
@@ -1,6 +1,6 @@
 <template>
-    <button type="button" onclick="my_modal_2.showModal()" class="text-black bg-[#e6e6e6] hover:bg-[#050708]/90 focus:ring-4 focus:outline-none focus:ring-[#050708]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-[#050708]/50 dark:hover:bg-[#050708]/30 mr-2 mb-2">
-        <img class="object-scale-down h-5 w-5 mr-2" src="src/assets/42logo.png">
+    <button type="button" onclick="my_modal_2.showModal()" class="text-black bg-[#e6e6e6] hover:bg-[#f2f2f2]/90 focus:ring-4 focus:outline-none focus:ring-[#f2f2f2]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-[#f2f2f2]/50 dark:hover:bg-[#f2f2f2]/30 mr-2 mb-2">
+        <img class="object-scale-down h-5 w-5 mr-2" src="src/assets/logo.svg">
         Sign up
       </button>
     <dialog id="my_modal_2" class="modal">
