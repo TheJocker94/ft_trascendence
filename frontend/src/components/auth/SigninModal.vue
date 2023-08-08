6e0717768b2af35a00a18b72dc86a11fe31e9d13
TheJocker94 <danielecastellano94@gmail.com>
2023-08-08 10:58:41 +0200

    validation login

--- frontend/src/components/auth/SigninModal.vue
+++ frontend/src/components/auth/SigninModal.vue
@@ -1,11 +1,11 @@
 <template>
     <button onclick="my_modal_1.showModal()" type="button" class="text-black bg-[#e6e6e6] hover:bg-[#050708]/90 focus:ring-4 focus:outline-none focus:ring-[#050708]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-[#050708]/50 dark:hover:bg-[#050708]/30 mr-2 mb-2">
     <img class="object-scale-down h-5 w-5 mr-2" src="src/assets/42logo.png">
-    Sign in Pincolino
+    Sign in
   </button>
     <dialog id="my_modal_1" class="modal">
-        <form @submit.prevent="login" method="dialog" class="modal-box">
-            <form class=" shadow-md rounded px-8 pt-6 pb-8 mb-4 ">
+        <form  method="dialog" class="modal-box">
+            <!-- <form @submit.prevent="login" class=" shadow-md rounded px-8 pt-6 pb-8 mb-4 ">
               <div class="mb-4">
                 <label class="block text-gray-700 text-sm font-bold mb-2" for="Emails">
                   Email
@@ -21,9 +21,6 @@
                 <p class="text-red-500 text-xs italic">Please choose a password.</p>
               </div>
               <div class="flex items-center justify-between">
-                <!-- <button @click="SendCredential()" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
-                  Sign In
-                </button> -->
                 <button @click="login" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit" >
                   Sign In
                 </button>
@@ -31,7 +28,25 @@
                   Sei scemo?
                 </a>
               </div>
-            </form>
+            </form> -->
+            <Form @submit="onSubmit" :validation-schema="schema" class=" shadow-md rounded px-8 pt-6 pb-8 mb-4 ">
+              <label class="block text-gray-700 text-sm font-bold mb-2" for="email">Your Email</label>
+              <Field v-model="credentials.email" class="shadow appearance-none border rounded w-full py-2 px-3 text-white-700 leading-tight focus:outline-none focus:shadow-outline" id="email_addr" name="email_addr" type="emailSig" />
+              <ErrorMessage name="email_addr" />
+          
+              <label class="block text-gray-700 text-sm font-bold mb-2" for="password_signin">Your Password</label>
+              <Field v-model="credentials.password" class="shadow appearance-none border rounded w-full py-2 px-3 text-white-700 leading-tight focus:outline-none focus:shadow-outline" id="password_signin" name="acc_pazzword" type="password" />
+              <ErrorMessage name="acc_pazzword" />
+              <div class="flex items-center justify-between">
+                <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit" >
+                  Sign In
+                </button>
+                <a class="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800" href="#">
+                  Sei scemo?
+                </a>
+              </div>
+              <!-- <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">Submit</button> -->
+            </Form>
             <p class="text-center text-gray-500 text-xs">
               &copy;2023 Mario Rispondi All rights reserved.
             </p>
@@ -39,15 +54,18 @@
         <form method="dialog" class="modal-backdrop">
           <button>close</button>
         </form>
+
       </dialog>
 </template>
 
 <script setup lang="ts">
 import { ref, reactive } from 'vue'
 import { useRouter } from 'vue-router';
-// import axios from 'axios';
-// import { useCurrentUserStore } from '@/stores/Current_User';
 import { useAuthStore } from '@/stores/auth';
+import { Form, Field, ErrorMessage } from 'vee-validate';
+import * as yup from 'yup';
+
+// Router, authStore, credentials
 const router = useRouter();
 const auth = ref(useAuthStore());
 const credentials = reactive({
@@ -55,17 +73,36 @@ const credentials = reactive({
   password:""
 })
 
-const login = async () => {
+//Validation schema
+const schema = yup.object().shape({
+  email_addr: yup.string().email().required().label('Email Address'),
+  acc_pazzword: yup.string().min(5).required().label('Your Password'),
+});
+//submit function
+// const login = async () => {
+//   const e = await auth.value.signInLocal(credentials.email, credentials.password);
+//   if (e)
+//   {
+//     console.log("danger", "failure", e.message);
+//     return;
+//   }
+//   else
+// 		router.push('/');
+// };
+async function onSubmit() {
+  console.log("Sommettiti")
+  console.log("email", credentials.email)
+  console.log("pass", credentials.password)
   const e = await auth.value.signInLocal(credentials.email, credentials.password);
   if (e)
-		{
-      console.log("danger", "failure", e.message);
-      return;
-    }
+  {
+    console.log("danger", "failure", e.message);
+    return;
+  }
   else
 		router.push('/');
-};
-
+  // alert(JSON.stringify(values, null, 2));
+}
 // const Current_User = reactive(useCurrentUserStore());
 // const SendCredential = () => {
 //     axios.post('http://localhost:3000/auth/local/signin', { email:credentials.email, password:credentials.password })
