28de5a6b8f82c4c60ae15196859bb8ae2c876b4b
TheJocker94 <danielecastellano94@gmail.com>
2023-08-14 10:45:51 +0200

    Added searchbar

---
+++ frontend/src/components/profile/SearchBar.vue
@@ -0,0 +1,64 @@
+<template>
+    <div>
+        <div  class="text-center">
+            <input v-model="searchQuery" @input="performSearch" placeholder="Search by username" class="input input-ghost" />
+            <ul v-if="searchResults.length > 0 && searchQuery">
+              <li v-for="result in searchResults" :key="result.id" @click="navigateToUser(result.id)">{{ result.username }}</li>
+            </ul>
+            <p v-else-if="searchQuery">No results found.</p>
+          </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+import { ref, watchEffect, computed } from 'vue';
+import { useRouter } from 'vue-router';
+import UserService from '@/services/UserService';
+import { useCurrentUserStore } from '@/stores/currentUser';
+import type { IUser } from '@/models/IUser';
+
+const props = defineProps({
+  idProfile: String,
+});
+
+const profile = ref<IUser>();
+let list = ref<IUser[]>();
+const router = useRouter();
+
+const currentUser = ref(useCurrentUserStore());
+async function fetchUsers() {
+  console.log("id new new ", props.idProfile);
+  profile.value = await UserService.getUserById(props.idProfile!);
+}
+
+// Using watchEffect to observe changes to props.idProfile
+watchEffect(async () => {
+  fetchUsers();
+  const pippo = await UserService.getUsers();
+  list.value = pippo;
+  const usernameIdArray: string[] = pippo.map(obj => `${obj.username}: ${obj.id}`);
+  console.log(usernameIdArray);
+  console.log("Current user", currentUser.value.avatar);
+});
+
+  // search bar
+const searchQuery = ref('');
+const searchResults = computed(() => {
+  const lowercaseQuery = searchQuery.value.toLowerCase();
+  if (list.value && Array.isArray(list.value)) {
+    return list.value.filter(obj => obj.username.toLowerCase().includes(lowercaseQuery));
+  }
+  return [];
+});
+const performSearch = () => {
+  // This function is triggered when the input value changes
+};
+const navigateToUser = (userId: string) => {
+  console.log("Id", userId);
+  router.push(`/users/${userId}`);
+};
+</script>
+
+<style scoped>
+
+</style>
--- frontend/src/views/ProfileView.vue
+++ frontend/src/views/ProfileView.vue
@@ -10,13 +10,7 @@
       </Suspense>
       <AvatarUserName :idProfile="userId"/>
       <!-- Search friend bar -->
-      <div  class="text-center">
-        <input v-model="searchQuery" @input="performSearch" placeholder="Search by username" class="input input-ghost" />
-        <ul v-if="searchResults.length > 0 && searchQuery">
-          <li v-for="result in searchResults" :key="result.id" @click="navigateToUser(result.id)">{{ result.username }}</li>
-        </ul>
-        <p v-else-if="searchQuery">No results found.</p>
-      </div>
+      <SearchBar :idProfile="userId"/>
 <!-- Friend list with avatar TODO -->
       <h4 class="text-sm text-center my-2 font-semibold text-white-700">Friends</h4>
       <div class="avatar-group -space-x-6 flex justify-center items-center">
@@ -52,46 +46,20 @@ import { useCurrentUserStore } from '@/stores/currentUser';
 import AvatarPic from '@/components/profile/AvatarPic.vue'
 import AvatarUserName from '@/components/profile/AvatarUserName.vue';
 import StatsComponent from '@/components/profile/StatsComponent.vue';
-import { ref, watchEffect, computed } from 'vue';
-import { useRouter, useRoute } from 'vue-router';
-import UserService from '@/services/UserService';
-import type { IUser } from '@/models/IUser';
+import SearchBar from '@/components/profile/SearchBar.vue';
+import { ref, watchEffect } from 'vue';
+import { useRoute } from 'vue-router';
 
-const router = useRouter();
 const route = useRoute();
 const userId = ref<string | string[]>();
-
 const currentUser = ref(useCurrentUserStore());
-let list = ref<IUser[]>();
-
 watchEffect(async () => {
   userId.value = route.params.userid;
   console.log("userId is ", userId.value);
 
   if (currentUser.value.userId)
     await currentUser.value.initStore(null, null);
-  
-  const pippo = await UserService.getUsers();
-  list.value = pippo;
-  const usernameIdArray: string[] = pippo.map(obj => `${obj.username}: ${obj.id}`);
-  console.log(usernameIdArray);
-  console.log("Current user", currentUser.value.avatar);
+
 });
 
-// search bar
-const searchQuery = ref('');
-const searchResults = computed(() => {
-  const lowercaseQuery = searchQuery.value.toLowerCase();
-  if (list.value && Array.isArray(list.value)) {
-    return list.value.filter(obj => obj.username.toLowerCase().includes(lowercaseQuery));
-  }
-  return [];
-});
-const performSearch = () => {
-  // This function is triggered when the input value changes
-};
-const navigateToUser = (userId: string) => {
-  console.log("Id", userId);
-  router.push(`/users/${userId}`);
-};
 </script>
